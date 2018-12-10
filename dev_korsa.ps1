4704216b88c6448c606ba546460c93c4fa49c7e0
Peter Korsbäck <peter.korsback@gmail.com>
2018-12-10 17:39:15 +0100

    Add script for korsa

---
+++ dev_korsa.ps1
@@ -0,0 +1,50 @@
+# Description: Boxstarter Script
+# Author: Microsoft
+# Common settings for web dev
+
+Disable-UAC
+
+# Get the base URI path from the ScriptToCall value
+$bstrappackage = "-bootstrapPackage"
+$helperUri = $Boxstarter['ScriptToCall']
+$strpos = $helperUri.IndexOf($bstrappackage)
+$helperUri = $helperUri.Substring($strpos + $bstrappackage.Length)
+$helperUri = $helperUri.TrimStart("'", " ")
+$helperUri = $helperUri.TrimEnd("'", " ")
+$helperUri = $helperUri.Substring(0, $helperUri.LastIndexOf("/"))
+$helperUri += "/scripts"
+write-host "helper script base URI is $helperUri"
+
+function executeScript {
+    Param ([string]$script)
+    write-host "executing $helperUri/$script ..."
+	iex ((new-object net.webclient).DownloadString("$helperUri/$script"))
+}
+
+#--- Setting up Windows ---
+executeScript "FileExplorerSettings.ps1";
+executeScript "SystemConfiguration.ps1";
+executeScript "CommonDevTools.ps1";
+executeScript "RemoveDefaultApps.ps1";
+executeScript "HyperV.ps1";
+executeScript "Docker.ps1";
+executeScript "WSL.ps1";
+executeScript "Browsers.ps1";
+
+#--- Tools ---
+code --install-extension msjsdiag.debugger-for-chrome
+code --install-extension msjsdiag.debugger-for-edge
+
+#--- Node
+choco install -y nodejs-lts # Node.js LTS, Recommended for most users
+choco install -y python2 # Node.js requires Python 2 to build native modules
+
+choco install -y visualstudio2017community --package-parameters="'--add Microsoft.VisualStudio.Component.Git'"
+Update-SessionEnvironment #refreshing env due to Git install
+
+#--- Microsoft WebDriver ---
+choco install -y microsoftwebdriver
+
+Enable-UAC
+Enable-MicrosoftUpdate
+Install-WindowsUpdate -acceptEula
